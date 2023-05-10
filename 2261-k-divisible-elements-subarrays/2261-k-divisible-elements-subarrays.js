f0737978fe92b0d4ea1ff5245871100fd4995a41
non <80571545+gaicuangua@users.noreply.github.com>
2023-05-10 22:49:46 +0700

    Time: 149 ms (98.46%), Space: 77.6 MB (32.31%) - LeetHub

--- 2261-k-divisible-elements-subarrays/2261-k-divisible-elements-subarrays.js
+++ 2261-k-divisible-elements-subarrays/2261-k-divisible-elements-subarrays.js
@@ -11,31 +11,28 @@ function TrieNode() {
 
 var countDistinct = function (nums, k, p) {
     const root = new TrieNode();
-    let cnt = 0, ans = 0;
-    const a = [];
-    const dfs = (curNode, i) => {
-        if (i >= a.length) return;
-        const val = a[i];
+    let cnt = 0, ans = 0, l = 0;
+    const dfs = (curNode, i, end) => {
+        if (i >= end) return;
+        const val = nums[i];
         if (!curNode.childs.has(val)) {
             ++ans;
             const newNode = new TrieNode();
             newNode.isEnd = true;
-            curNode.childs.set(val, newNode);
+            curNode.childs.set(val, newNode, end);
         }
-        dfs(curNode.childs.get(val), i + 1);
+        dfs(curNode.childs.get(val), i + 1, end);
     }
-    for (let i = 0; i < nums.length; i++) {
-        const val = nums[i];
+    for (let r = 0; r < nums.length; r++) {
+        const val = nums[r];
         cnt += val % p ? 0 : 1;
-        if (cnt > k) {
-            for(let j = 0; j < a.length; ++j) dfs(root, j);
-            while (cnt > k) {
-                const top = a.shift();
-                cnt -= top % p ? 0 : 1;
-            }
+        if (cnt <= k) continue;
+        for (let j = l; j < r; ++j) dfs(root, j, r);
+        while (cnt > k) {
+            const num = nums[l++];
+            cnt -= num % p ? 0 : 1;
         }
-        a.push(val);
     }
-    for(let j = 0; j < a.length; ++j) dfs(root, j);
+    for (let j = l; j < nums.length; ++j) dfs(root, j, nums.length);
     return ans;
 };
