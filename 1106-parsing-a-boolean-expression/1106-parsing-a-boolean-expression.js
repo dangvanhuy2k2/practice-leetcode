f5fdc23a166fe78d3cdc3d162a8059489a996dce
Đặng Văn Huy <80571545+dangvanhuy2k2@users.noreply.github.com>
2023-06-09 17:23:36 +0700

    Time: 109 ms (26.67%), Space: 49 MB (40.00%) - LeetHub

---
+++ 1106-parsing-a-boolean-expression/1106-parsing-a-boolean-expression.js
@@ -0,0 +1,36 @@
+/**
+ * @param {string} expression
+ * @return {boolean}
+ */
+var parseBoolExpr = function (expression) {
+    const hasExpress = (str) => {
+        for (const c of str) {
+            if (c === 'f' || c === 't') continue;
+            return true;
+        }
+        return false;
+    }
+    const dfs = (expression) => {
+        const len = expression.length;
+        if (len === 1) return expression === "f" ? false : true;
+        const c = expression.charAt(0);
+        const str = expression.substring(2, len - 1) + ",";
+        const bools = [];
+        let cnt = 0;
+        let s = "";
+        for (let c of str) {
+            if (c === ',' && cnt === 0) {
+                bools.push(dfs(s));
+                s = "";
+                continue;
+            }
+            s += c;
+            if (c === ")" || c === "(") cnt += c === ")" ? -1 : 1;
+        }
+        if (c === "!") return !bools[0];
+        if (c === "|") return bools.includes(true);
+        return bools.every(b => b);
+    }
+    const ans = dfs(expression);
+    return ans;
+};
