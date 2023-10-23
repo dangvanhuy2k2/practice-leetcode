534b1bcf154b0137e5f49e0abde6c8d2549062ce
Đặng Văn Huy <80571545+dangvanhuy2k2@users.noreply.github.com>
2023-10-23 15:47:51 +0700

    Time: 63 ms (40.03%), Space: 42.4 MB (94.24%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.js
+++ 0342-power-of-four/0342-power-of-four.js
@@ -2,16 +2,10 @@
  * @param {number} n
  * @return {boolean}
  */
-var isPowerOfFour = function(n) {
-     if(n <= 0) return false;
-     let l = 0 , r = 31;
-     while(l <= r) {
-        const m = Math.floor((l + r) / 2);
-        const num = 4 ** m;
-        if(num === n) return true;
-        else if(num < n) {
-            l = m + 1;
-        } else r = m - 1;
-     }  
-     return false;
+var isPowerOfFour = function (n) {
+    while (n > 1) {
+        if (n % 4) return false;
+        n /= 4;
+    }
+    return n === 1;
 };
