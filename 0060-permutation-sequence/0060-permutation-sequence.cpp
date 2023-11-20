d8b8f79ab4df88a7201ffe2c982a44c9d162ee06
Huy <80571545+dangvanhuy2k2@users.noreply.github.com>
2023-11-20 22:34:03 +0700

    Time: 98 ms (22.51%), Space: 6.3 MB (75.47%) - LeetHub

---
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -0,0 +1,15 @@
+class Solution
+{
+public:
+    string getPermutation(int n, int k)
+    {
+        string str = "";
+        for (int i = 1; i <= n; ++i)
+            str += to_string(i);
+        int cnt = 1;
+        for(int i = 1; i < k; ++i) {
+            next_permutation(str.begin(), str.end());
+        }
+        return str;
+    }
+};
