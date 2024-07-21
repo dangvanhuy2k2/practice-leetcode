2fd0f63f69c48a456d4ff4eb48998512c9541051
Huy <80571545+dangvanhuy2k2@users.noreply.github.com>
2024-07-21 12:12:59 +0700

    Time: 264 ms (72.68%), Space: 89.8 MB (73.17%) - LeetHub

--- 3212-count-submatrices-with-equal-frequency-of-x-and-y/3212-count-submatrices-with-equal-frequency-of-x-and-y.js
+++ 3212-count-submatrices-with-equal-frequency-of-x-and-y/3212-count-submatrices-with-equal-frequency-of-x-and-y.js
@@ -6,34 +6,29 @@ var numberOfSubmatrices = function (grid) {
     const n = grid.length,
         m = grid[0].length;
     let ans = 0;
-    const p = Array.from({ length: n }, () =>
-        Array.from({ length: m }, () => [0, 0])
-    );
+    const p = Array.from({ length: n }, () => Array(m).fill(0));
+    const hasX = Array.from({ length: n }, () => Array(m).fill(false));
+    let isNotEmpty = false;
     for (let i = 0; i < n; ++i) {
         for (let j = 0; j < m; ++j) {
-            const c = grid[i][j];
-            let x = 0,
-                y = 0;
-            if (c === "X") x = 1;
-            else if (c === "Y") y = 1;
-            if (i === 1 && j === 0) {
-                console.log("log: ", p);
-            }
+            const val = grid[i][j];
+            if (val === "X") hasX[i][j] = true;
+            isNotEmpty = isNotEmpty || val !== ".";
+            let tmp = val === "X" ? 1 : val === "Y" ? -1 : 0;
             if (i - 1 >= 0) {
-                x += p[i - 1][j][0];
-                y += p[i - 1][j][1];
+                tmp += p[i - 1][j];
+                hasX[i][j] |= hasX[i - 1][j];
             }
             if (j - 1 >= 0) {
-                x += p[i][j - 1][0];
-                y += p[i][j - 1][1];
+                tmp += p[i][j - 1];
+                hasX[i][j] |= hasX[i][j - 1];
             }
             if (i - 1 >= 0 && j - 1 >= 0) {
-                x -= p[i - 1][j - 1][0];
-                y -= p[i - 1][j - 1][1];
+                tmp -= p[i - 1][j - 1];
+                hasX[i][j] |= hasX[i - 1][j - 1];
             }
-            p[i][j][0] = x;
-            p[i][j][1] = y;
-            ans += x === y && x > 0 ? 1 : 0;
+            p[i][j] = tmp;
+            ans += !tmp && hasX[i][j] ? 1 : 0;
         }
     }
     return ans;
